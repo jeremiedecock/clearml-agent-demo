5ab3b913f606124c2e24bce427bb15d5897ee8e5
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-05-31 21:11:04 +0200

    Add a podman-run script.

---
+++ run-clearml-agent-worker-bi-gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# To use Nvidia GPUs with Podman, a CDI specification of the installed device(s) have to be made first:
+# 1. Generate the CDI specification file: `sudo nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml`
+# 2. (Optional) Check the names of the generated devices: `nvidia-ctk cdi list`
+#
+# More info: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/cdi-support.html#procedure
+
+podman run --rm -it \
+           --name="clearml-agent-worker-bi-gpu" \
+           -e CLEARML_AGENT_PACKAGE_PYTORCH_RESOLVE=none \
+           -v clearml-agent-cache-worker-bi-gpu:/root/.clearml \
+           localhost/clearml-agent:latest clearml-agent daemon --queue "worker-bi-gpu" --foreground
