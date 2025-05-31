c2a37e398020131e774016a692712a99218874ea
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-05-31 20:48:38 +0200

    Fix several bugs (seems ok now).

--- run-clearml-agent-worker-cpu.sh
+++ run-clearml-agent-worker-cpu.sh
@@ -6,7 +6,8 @@
 #
 # More info: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/cdi-support.html#procedure
 
-
 podman run --rm -it \
-           -v clearml-agent-cache-worker-cpu:/root/.clearml/cache \
+           --name="clearml-agent-worker-cpu" \
+           -e CLEARML_AGENT_PACKAGE_PYTORCH_RESOLVE=none \
+           -v clearml-agent-cache-worker-cpu:/root/.clearml \
            localhost/clearml-agent:latest clearml-agent daemon --queue "worker-cpu" --foreground
